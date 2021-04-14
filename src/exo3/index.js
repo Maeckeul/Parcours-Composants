da6423ea9e4b3ddb653514aad357f44c59cef306
Maeckeul <cmaeckeul@gmail.com>
2021-04-13 21:39:58 -1000

    :alembic: exo 3 finished

--- src/exo3/index.js
+++ src/exo3/index.js
@@ -32,44 +32,23 @@ import { render } from 'react-dom';
 import test from './test';
 
 
+
 /*
  * Code
  */
 
-
-// un composant pourra recevoir un paramètre, qu'on appelle par habitude props qui contiendra un objet contenant les associations de clés et de valeur passer à l'instanciation/execution
-// const HelloWorld = (props) => {
-//   // props -> { method: 'Gros minet', lang: 'Tit' }
-//   // dans du JSX on peut repasser "en mode interprété" en écrivant entre accolades
-//   return (
-//     <div id="hello-world">
-//       <h1>Ce HTML a été généré par {props.method}</h1>
-//       <h2>Et affiché avec {props.lang} {1 + 2}</h2>
-//     </div>
-//   );
-// };
-
-// avec de la destructuration
-const HelloWorld = ({ method, lang }) => (
-  <div id="hello-world">
-    <h1>Ce HTML a été généré par {method}</h1>
-    <h2>Et affiché avec {lang}</h2>
-  </div>
-);
-
-
-// appel d'un composant avec un paramètre
-// version classlique
-// HelloWorld({
-//   method: 'Gros minet',
-//   lang: 'Titi',
-// });
-// // version JSX
-// <HelloWorld method="Gros minet" land="Titi" />
-
+const HelloWorld = ({ lang, method }) => {
+  return (
+    <div id="hello-world">
+      <h1>Ce HTML a été généré par {lang}</h1>
+      <h2>Et affiché avec {method}</h2>
+    </div>
+  );
+};
 
 const rootDomElement = document.getElementById('root');
-render(<HelloWorld method="React" lang="React-Dom" />, rootDomElement);
+
+render(<HelloWorld lang="React" method="ReactDOM" />, rootDomElement);
 
 /*
  * Tests
